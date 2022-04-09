6f013deec99b967569765ea75d53f22235cf40c2
RayDLC <theraydavid@gmail.com>
2022-04-09 18:53:44 -0500

    Cambios Semana03

--- app/src/main/java/com/cibertec/proyecto/entity/Sala.java
+++ app/src/main/java/com/cibertec/proyecto/entity/Sala.java
@@ -2,4 +2,11 @@ package com.cibertec.proyecto.entity;
 
 public class Sala {
 
+    private int idSala;
+    private String numero;
+    private int piso;
+    private int capacidad;
+    private String recursos;
+
+
 }
