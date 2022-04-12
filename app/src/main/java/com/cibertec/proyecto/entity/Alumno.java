6ade6670c7e4ac589145db8ab0464b317ca04f8b
=edupar <edujosparbar2002@gmail.com>
2022-04-12 11:40:48 -0500

    Registrar Alumno Eduardo Josue Paredes Barriga T5AN

--- app/src/main/java/com/cibertec/proyecto/entity/Alumno.java
+++ app/src/main/java/com/cibertec/proyecto/entity/Alumno.java
@@ -2,6 +2,85 @@ package com.cibertec.proyecto.entity;
 
 public class Alumno {
 
+    private int idAlumno;
+    private String nombres;
+    private String apellidos;
+    private int dni;
+    private String direccion;
+    private String correo;
+    private String fechaNacimiento;
+    private String fechaRegistro;
+    private int estado;
 
+    public int getIdAlumno() {
+        return idAlumno;
+    }
 
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(String fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
 }
--- app/src/main/java/com/cibertec/proyecto/service/ServiceAlumno.java
+++ app/src/main/java/com/cibertec/proyecto/service/ServiceAlumno.java
@@ -1,4 +1,14 @@
 package com.cibertec.proyecto.service;
 
+import com.cibertec.proyecto.entity.Alumno;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 public interface ServiceAlumno {
+
+
+    @POST("alumno")
+    public abstract Call<Alumno> registraAlumno(@Body Alumno obj);
 }
