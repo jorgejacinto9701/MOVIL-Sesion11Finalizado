e4e75695bea33c89d3d98ce4294ad1f7e9ba797f
AlanisGR <luzalanisgr@gmail.com>
2022-04-27 16:50:19 -0500

    Registro de revista completo

--- app/src/main/java/com/cibertec/proyecto/service/ServiceRevista.java
+++ app/src/main/java/com/cibertec/proyecto/service/ServiceRevista.java
@@ -1,4 +1,13 @@
 package com.cibertec.proyecto.service;
 
+import com.cibertec.proyecto.entity.Revista;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 public interface ServiceRevista {
+    @POST("revista")
+    public abstract Call<Revista> registraRevista(@Body Revista obj);
+
 }
