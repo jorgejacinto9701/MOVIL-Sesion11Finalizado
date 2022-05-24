7bdf174966364748caf99136f8442aa302094aad
ProjectsByBlue <leonardolopezpeve@hotmail.com>
2022-05-24 08:04:13 -0500

    Consulta de libro (Service,Adapter, Views y Activity)

--- app/src/main/java/com/cibertec/proyecto/adapter/LibroAdapter.java
+++ app/src/main/java/com/cibertec/proyecto/adapter/LibroAdapter.java
@@ -1,9 +1,16 @@
 package com.cibertec.proyecto.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import androidx.annotation.NonNull;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.cibertec.proyecto.R;
 import com.cibertec.proyecto.entity.Libro;
 import java.util.List;
 
@@ -18,4 +25,24 @@ public class LibroAdapter extends ArrayAdapter<Libro>  {
         this.lista = lista;
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.activity_libro_crud_item, parent, false);
+
+        Libro obj = lista.get(position);
+
+        TextView txtId = row.findViewById(R.id.idItemIdLibro);
+        txtId.setText(String.valueOf(obj.getIdLibro()));
+
+        TextView txtTitulo = row.findViewById(R.id.idItemTituloLibro);
+        txtTitulo.setText(obj.getTitulo());
+
+        TextView txtCategoria = row.findViewById(R.id.idItemCategoriaLibro);
+        txtCategoria.setText(obj.getCategoria());
+
+        return row;
+    }
 }
